1cf48533aa264144d7ff4d2c07f0148d6e5cef0d
HariCharan <haricharankonduri@gmail.com>
2018-01-31 10:15:59 +0530

    Added basic numerical system binary, decimal, hexa, utf8

---
+++ numerical-systems/3_hexadecimal/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("%d - %b - %x \n", 33, 33, 33)
+	fmt.Printf("%d - %b - %#x \n", 33, 33, 33)
+	fmt.Printf("%d - %b - %#X", 33, 33, 33)
+	
+
+}
